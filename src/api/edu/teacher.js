f16867f7f1dc559f9280cfb0a0e2658ec313a293
文 <1456043860@qq.com>
2023-07-09 01:34:12 +0800

    7/9 oss上传头像功能前端代码

--- src/api/edu/teacher.js
+++ src/api/edu/teacher.js
@@ -4,7 +4,7 @@ export default {
 //   讲师列表（条件查询分页）
   getTeacherListPage(current,limit,teacherQuery){
     return request({
-      url: `/edu/teacher/pageTeacherCondition/${current}/${limit}`,
+      url: `/eduservice/teacher/pageTeacherCondition/${current}/${limit}`,
       method: 'post',
     //   teacherQuery条件的对象，后端使用RequestBody获取数据
     //   data表示把对象转换json进行传递到接口里面
@@ -14,14 +14,14 @@ export default {
   // 删除
   removeById(id){
     return request({
-      url: `/edu/teacher/${id}`,
+      url: `/eduservice/teacher/${id}`,
       method: 'delete'
     })
   },
   //添加讲师
   addTeacher(teacher){
     return request({
-      url: `/edu/teacher/addTeacher`,
+      url: `/eduservice/teacher/addTeacher`,
       method: 'post',
       data:teacher
     })
@@ -29,14 +29,14 @@ export default {
   //根据id查询讲师，修改需要调用此接口，实现数据回显
   getTeacherInfo(id){
     return request({
-      url: `/edu/teacher/getTeacher/${id}`,
+      url: `/eduservice/teacher/getTeacher/${id}`,
       method: 'get',
     })
   },
   //修改讲师
   updateTeacherInfo(teacher){
     return request({
-      url: `/edu/teacher/updateTeacher`,
+      url: `/eduservice/teacher/updateTeacher`,
       method: 'post',
       data:teacher
     })
